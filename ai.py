239ab2b97af5e05645b97d01df87ab3f12dfd03c
ShafiSahal99 <shafisahal99@gmail.com>
2021-05-12 17:17:25 +0530

    Completed AI

--- ai.py
+++ ai.py
@@ -114,3 +114,22 @@ class MA:
         return np.mean(self.list_of_rewards)
     
 ma = MA(100)
+
+loss = nn.MSELoss()
+optimizer = opptim.Adam(cnn.parameters(), lr = 0.001)
+nb_epochs = 100
+
+for epoch in range (1, nb_epochs + 1):
+    memory.run_steps(200)
+    for batch in memory.sample_batch(128):
+        inputs, targets = eligibility_trace(batch)
+        inputs, targets = Variable(inputs), Variable(targets)
+        predictions = cnn(inputs)
+        loss_error = loss(predictions, target)
+        optimizer.zero_grad()
+        loss_error.backward()
+        optimizer.step()
+    rewards_step = n_steps.rewards_steps()
+    ma.add(rewards_steps)
+    avg_reward = ma.average()
+    print("Epoch: %s, Average Reward: %s" % (str(epoch), str(avg_reward)))
